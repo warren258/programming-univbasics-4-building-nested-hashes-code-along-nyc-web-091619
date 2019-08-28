0f529ad25b28ee3d552812eb047f146ac1765b52
Warren McDonald <warrenjmac@mail.com>
2019-08-28 23:00:02 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague",age: "53"},
-      :matriarch => {name: "Lady Montigue",age: "54"},
+      :matriarch => {name: "Lady Montague",age: "54"},
       :hero => {name: "Romeo",age: "15",status: "alive"},
       :hero_friends => [
         {
