801bed64c4237aaceb58295d4e7db5c3808a8c90
Warren McDonald <warrenjmac@mail.com>
2019-08-28 22:42:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,18 @@ def third_challenge
         age: = "15",
         status: = "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {
+          name: "Belvolio",
+          age: "17",
+          attitude: "worried"
+        }
+        {
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
+        }
+        ]
    }, 
    :capulet => {
       :patriarch => {
@@ -30,7 +41,18 @@ def third_challenge
         age: = "15",
         status: = "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        }
+        {
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
+        }
+        ]
    }
   }
 
